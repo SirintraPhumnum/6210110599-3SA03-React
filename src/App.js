01c27b9053d5ef5b79a1d428dc38e180179e8e5a
SirintraPhumnum <6210110599@psu.ac.th>
2022-08-15 17:48:21 +0700

    Part4_Components&Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import WordCard from './WordCard';
+
+const word = "Hello";
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      {
+        <WordCard value={word}/>
+      }
     </div>
   );
 }
---
+++ src/CharacterCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function CharacterCard(props)
+{
+    return (
+        <div className="card">{props.value}</div>
+    )
+}
