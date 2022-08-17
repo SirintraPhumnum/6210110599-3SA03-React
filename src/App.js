9ba6437c3f855c573b4dcdb261338dd566b89edd
SirintraPhumnum <6210110599@psu.ac.th>
2022-08-17 21:43:08 +0700

    Part7

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 
 import WordCard from './WordCard';
+import NewWord from './NewWord';
 
 const word = "Hello";
+const newword = "Monday";
+
 function App() {
   return (
     <div>
-      {
         <WordCard value={word}/>
-      }
+        <NewWord value={newword}/>
     </div>
   );
 }
